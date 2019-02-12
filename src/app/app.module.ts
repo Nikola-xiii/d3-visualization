972acc21155fb06f8fb7625207b0b1368f8f9cac
Nikola Serdiuk <nikola.serdiuk@gmail.com>
2019-02-12 17:11:10 +0200

    Revert "5 interactive map"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,34 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatGridListModule, MatCardModule,
-  MatMenuModule, MatIconModule, MatButtonModule,
-  MatToolbarModule, MatSidenavModule, MatListModule, MatSelectModule } from '@angular/material';
-import { LayoutModule } from '@angular/cdk/layout';
-import { GeoMapDemoComponent } from './pages/geo-map-demo/geo-map-demo.component';
-import { HttpClientModule, HttpClientJsonpModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    GeoMapDemoComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
-    MatGridListModule,
-    MatSelectModule,
-    MatCardModule,
-    MatMenuModule,
-    MatIconModule,
-    MatButtonModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatSidenavModule,
-    MatListModule,
-    HttpClientJsonpModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
